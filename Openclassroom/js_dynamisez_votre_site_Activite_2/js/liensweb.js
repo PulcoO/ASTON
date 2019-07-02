50aa32c8fd62cdc25ee8c97dd432e29f9915b00e
Mathieu Veys <oominimathoo@gmail.com>
2019-07-02 16:47:55 +0200

    add open activity

---
+++ Openclassroom/js_dynamisez_votre_site_Activite_2/js/liensweb.js
@@ -0,0 +1,70 @@
+//////////////////// Activité 2 ///////////////////
+
+//////// buttons /////////
+
+document.querySelector("#btn_form").addEventListener('click', function(){
+    document.querySelector(".form").style.display = "block"
+    document.querySelector("#btn_form").style.display = "none";
+})
+
+document.querySelector("#btn_add").addEventListener("click", function(){
+    document.querySelector(".form").style.display = "none";
+    
+})
+
+
+
+var listeLiens = [
+    {
+        titre: "So Foot",
+        url: "http://sofoot.com",
+        auteur: "yann.usaille"
+    },
+    {
+        titre: "Guide d'autodéfense numérique",
+        url: "http://guide.boum.org",
+        auteur: "paulochon"
+    },
+    {
+        titre: "L'encyclopédie en ligne Wikipedia",
+        url: "http://Wikipedia.org",
+        auteur: "annie.zette"
+    }
+];
+
+const section = document.querySelector("#contenu");
+
+listeLiens.forEach(element => {
+
+    let elementA = document.createElement("a");
+    let titre = document.createTextNode(element.titre + " " );
+    elementA.appendChild(titre);
+    elementA.style.color = "#428bca";
+    elementA.style.textDecoration = "none";
+    
+    
+   
+    let elementSpan = document.createElement("span");
+    let elementBr = document.createElement("br");
+    let url = document.createTextNode(element.url + "/n");
+    let auteur = document.createTextNode("Ajouté par : " + element.auteur);
+
+    elementA.href = url.textContent;
+
+
+    elementSpan.appendChild(url);
+    elementSpan.appendChild(elementBr);
+    elementSpan.appendChild(auteur);
+
+
+    let div = document.createElement("div");
+    div.className = "lien";
+    div.appendChild(elementA);
+    div.appendChild(elementSpan)
+    section.appendChild(div);
+    
+
+});
+
+
+// TODO : compléter ce fichier pour ajouter les liens à la page web
