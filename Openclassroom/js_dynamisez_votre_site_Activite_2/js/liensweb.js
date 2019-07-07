cd86198b27cb734bb2a83213db8456d5e44b1a44
PulcoOx - Home <oominimathoo@gmail.com>
2019-07-07 20:49:43 +0200

    Home add aston activité 2 & calculatrice

--- Openclassroom/js_dynamisez_votre_site_Activite_2/js/liensweb.js
+++ Openclassroom/js_dynamisez_votre_site_Activite_2/js/liensweb.js
@@ -1,21 +1,6 @@
 //////////////////// Activité 2 ///////////////////
 
-//////// buttons /////////
-
-document.querySelector("#btn_form").addEventListener('click', function(){
-    document.querySelector(".form").style.display = "block"
-    document.querySelector("#btn_form").style.display = "none";
-})
-
-document.querySelector("#btn_add").addEventListener("click", function(){
-    document.querySelector(".form").style.display = "none";
-    
-})
-
-
-
-var listeLiens = [
-    {
+var listeLiens = [{
         titre: "So Foot",
         url: "http://sofoot.com",
         auteur: "yann.usaille"
@@ -31,40 +16,77 @@ var listeLiens = [
         auteur: "annie.zette"
     }
 ];
-
 const section = document.querySelector("#contenu");
+creationOfElement(listeLiens)
+    //////// buttons /////////
 
-listeLiens.forEach(element => {
+document.querySelector("#btn_form").addEventListener('click', function() {
+    document.querySelector(".form").style.display = "block"
+    document.querySelector("#btn_form").style.display = "none";
+})
 
-    let elementA = document.createElement("a");
-    let titre = document.createTextNode(element.titre + " " );
-    elementA.appendChild(titre);
-    elementA.style.color = "#428bca";
-    elementA.style.textDecoration = "none";
-    
-    
-   
-    let elementSpan = document.createElement("span");
-    let elementBr = document.createElement("br");
-    let url = document.createTextNode(element.url + "/n");
-    let auteur = document.createTextNode("Ajouté par : " + element.auteur);
+document.querySelector("#btn_add").addEventListener("click", function() {
+    document.querySelector(".form").style.display = "none";
+    document.querySelector("#btn_form").style.display = "block";
+    document.querySelector(".done").style.display = "block";
+    setTimeout(function() {
+        document.querySelector(".done").style.display = "none";
+    }, 2000);
+    let newTitre = document.querySelector(".titre").value;
+    let newUrl = document.querySelector(".url").value;
+    // newUrl = validateText(newUrl);
+    let newAuteur = document.querySelector(".auteur").value;
 
-    elementA.href = url.textContent;
+    let newLiens = [{
+        titre: newTitre,
+        url: newUrl,
+        auteur: newAuteur
+    }]
+
+    creationOfElement(newLiens);
+})
+
+function validateText(url) {
+    if (url.indexOf("http://") == 0 || url.indexOf("https://") == 0) {
+        let regexUrl = "https://" + url;
+        return regexUrl;
+    }
+}
+
+function creationOfElement(array) {
+    array.forEach(element => {
+
+        let elementA = document.createElement("a");
+        let titre = document.createTextNode(element.titre + " ");
+        elementA.appendChild(titre);
+        elementA.style.color = "#428bca";
+        elementA.style.textDecoration = "none";
 
 
-    elementSpan.appendChild(url);
-    elementSpan.appendChild(elementBr);
-    elementSpan.appendChild(auteur);
+
+        let elementSpan = document.createElement("span");
+        let elementBr = document.createElement("br");
+        let url = document.createTextNode(element.url);
+        let auteur = document.createTextNode("Ajouté par : " + element.auteur);
+
+        elementA.href = url.textContent;
 
 
-    let div = document.createElement("div");
-    div.className = "lien";
-    div.appendChild(elementA);
-    div.appendChild(elementSpan)
-    section.appendChild(div);
-    
+        elementSpan.appendChild(url);
+        elementSpan.appendChild(elementBr);
+        elementSpan.appendChild(auteur);
 
-});
+
+        let div = document.createElement("div");
+        div.className = "lien";
+        div.appendChild(elementA);
+        div.appendChild(elementSpan)
+        section.appendChild(div);
+
+
+    });
+}
+
 
 
 // TODO : compléter ce fichier pour ajouter les liens à la page web
