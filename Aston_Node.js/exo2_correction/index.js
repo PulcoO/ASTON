63d8f7d03d14b6061a9a706ba5df75494830a01c
PulcoOx - Home <oominimathoo@gmail.com>
2019-07-08 21:08:42 +0200

    relou cette histoire

--- Aston_Node.js/exo2_correction/index.js
+++ Aston_Node.js/exo2_correction/index.js
@@ -1,5 +1,5 @@
-class Todo{
-    constructor(nom, contenu, dateCrea, date, fini, archive){
+class Todo {
+    constructor(nom, contenu, dateCrea, date, fini, archive) {
         this.nom = nom;
         this.contenu = contenu;
         this.dateCrea = dateCrea;
@@ -7,78 +7,95 @@ class Todo{
         this.fini = fini;
         this.archive = archive;
     }
-    getNom(){
+    getNom() {
         return this.nom;
     }
-    getContenu(){
+    getContenu() {
         return this.contenu;
     }
-    getDateCrea(){
+    getDateCrea() {
         return this.dateCrea;
     }
-    getDate(){
+    getDate() {
         return this.date;
     }
-    getFini(){
+    getFini() {
         return this.fini;
     }
-    getArchive(){
+    getArchive() {
         return this.archive;
     }
-    setNom(parametre){
+    setNom(parametre) {
         this.nom = parametre;
     }
-    setContenu(parametre){
+    setContenu(parametre) {
         this.contenu = parametre;
     }
-    setDateCrea(parametre){
+    setDateCrea(parametre) {
         this.dateCrea = parametre;
     }
-    setDate(parametre){
+    setDate(parametre) {
         this.date = parametre;
     }
-    setFini(parametre){
+    setFini(parametre) {
         this.fini = parametre;
     }
-    setArchive(parametre){
+    setArchive(parametre) {
         this.archive = parametre;
     }
 }
 
 let list = [
-    new Todo('chercher du pain','Au super boulanger du coin',new Date(), '2019-08-21', false, false),
-    new Todo('Faire des courses','A Hyper U',new Date(), '2019-10-21', false, false),
+    new Todo('chercher du pain', 'Au super boulanger du coin', new Date(), '2019-08-21', false, false),
+    new Todo('Faire des courses', 'A Hyper U', new Date(), '2019-10-21', false, false),
 ];
+
+let archive = [];
 let cible = document.getElementById('cible');
 
-function affichage(){
+function affichage() {
     cible.innerHTML = ''; //je vide ma div cible
-    for(i = 0; i < list.length ; i++){
-        cible.innerHTML +='<div class="row my-3"><div class="col-1"><button class="btn btn-danger btn-sm" onclick="supprime('+i+')">X</button></div><div class="col-4">'+list[i].nom+'</div><div class="col-3">'+list[i].date+'</div><div class="col-2"><input type="checkbox" value="'+list[i].fini+'"></div><div class="col-2"><input type="checkbox" class="archive" value="'+list[i].archive+'"></div></div>';
+    for (i = 0; i < list.length; i++) {
+        cible.innerHTML += '<div class="row my-3"><div class="col-1"><button class="btn btn-danger btn-sm" onclick="supprime(' + i + ')">X</button></div><div class="col-4">' + list[i].nom + '</div><div class="col-3">' + list[i].date + '</div><div class="col-2"><input type="checkbox" value="' + list[i].fini + '"></div><div class="col-2"><input type="checkbox" class="archive" onclick="checkArchive()" value="' + list[i].archive + '"></div></div>';
     }
 }
 
-function add(){
+function add() {
     let nom = document.getElementById('nom');
     let date = document.getElementById('date');
-    list.push(new Todo(nom.value,'',new Date(),date.value,false,false));
+    list.push(new Todo(nom.value, '', new Date(), date.value, false, false));
     nom.value = '';
     date.value = '';
     affichage();
 }
 
-function check() {
-    document.getElementById("archive").checked = true;
-}
 
-function uncheck() {
-    document.getElementById("archive").checked = false;
-}
-
-function supprime(i){
-    list.splice(i,1);
+function supprime(i) {
+    list.splice(i, 1);
     affichage();
 }
-document.getElementById('envoi').addEventListener('click',add,false);
+document.getElementById('envoi').addEventListener('click', add, false);
+
+// // on s'occupe du checkbox archive
+
+
+
+
+
+function checkArchive() {
+    let checkbox = document.querySelector('.archive');
+    console.log(checkbox);
+    console.log(checkbox.value);
+    if (checkbox.checked) {
+        checkbox.value = true;
+        console.log(checkbox.value);
+        console.log(checkbox.parentNode.parentNode.textContent)
+    } else if (!checkbox.checked) {
+        checkbox.value = false;
+        console.log(checkbox.value)
+    }
+
+}
+
 
 affichage();
